f817a0e23baa0ec89f6356e3139374f5c9f98e49
ianngure <kayngure57@gmail.com>
2024-08-02 22:23:01 +0300

    feat: added navbar.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <App />,
-  }
-]);
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router}>
+  <Router>
     <App />
-  </RouterProvider>
+  </Router>
 );
