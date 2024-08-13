36b0e85e6b672c6b83ae3acabe25141e44bc0e68
victor <wahinyavictor818@gmail.com>
2024-08-13 17:00:37 +0300

    add css

--- src/Components/Admin Dashboard/Home.jsx
+++ src/Components/Admin Dashboard/Home.jsx
@@ -196,3 +196,27 @@ const Dashboard = () => {
                 legends: { text: { fill: colors.grey[100] } },
                 tooltip: { container: { color: colors.primary[500] } },
               }}
+              keys={["quantity"]}
+              indexBy="product"
+              margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+              padding={0.3}
+              colors={{ scheme: "nivo" }}
+              borderColor={{ from: "color", modifiers: [["darker", "1.6"]] }}
+              axisTop={null}
+              axisRight={null}
+              axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Product",
+                legendPosition: "middle",
+                legendOffset: 32,
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Units Sold",
+                legendPosition: "middle",
+                legendOffset: -40,
+              }}
